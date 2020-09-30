9203bf73c2a3e53c57b15684c623ec43619d49ef
turtlez23 <mil.mazur@gmail.com>
2020-09-30 11:43:40 +0200

    Add sort with file date creation

--- tkmservices/views_printcard_api.py
+++ tkmservices/views_printcard_api.py
@@ -7,7 +7,7 @@ import json
 import requests
 
 from django.conf import settings
-from django.shortcuts import render, redirect 
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 from django.http import FileResponse
@@ -42,35 +42,40 @@ PHOTO_W = 13*mm
 PHOTO_H = 17*mm
 FOREIGNER_ID_LENGTH = len('2000-05-27')
 
-def tkmGetCustomerFormData(startDate, endDate, variant, online):          
-    # "CLOSED"        
+
+def tkmGetCustomerFormData(startDate, endDate, variant, online):
+    # "CLOSED"
     # "TO_PERSONALIZE"
-    # "REJECTED"      
-    # "PERSONALIZED"  
-    # "NEW"           
-    url = settings.TKM_WEBSERVICE_ADDRESS + 'tkmGetCustomerRequest_to_personalize_' + settings.TKM_WEBSERVICE_VERSION
+    # "REJECTED"
+    # "PERSONALIZED"
+    # "NEW"
+    url = settings.TKM_WEBSERVICE_ADDRESS + \
+        'tkmGetCustomerRequest_to_personalize_' + settings.TKM_WEBSERVICE_VERSION
     params = {
-        "queryParams":[{
+        "queryParams": [{
             "name": "online",
-            "value": str(online)},{
+            "value": str(online)}, {
             "name": "variant",
-            "value": str(variant)},{
+            "value": str(variant)}, {
             "name": "startdate",
-            "value": startDate},{
+            "value": startDate}, {
             "name": "enddate",
-            "value": endDate},{
+            "value": endDate}, {
             "name": "key",
             "value": settings.TKM_WEBSERVICES_AUTH_KEY}
         ]
     }
-    headers={'Content-type':'application/json', 'Accept':'application/json'}
-    req = requests.post(url=url, data=json.dumps(params), headers=headers, auth=(settings.TKM_WEBSERVICE_USER_LOGIN, settings.TKM_WEBSERVICE_USER_PASS))
+    headers = {'Content-type': 'application/json',
+               'Accept': 'application/json'}
+    req = requests.post(url=url, data=json.dumps(params), headers=headers, auth=(
+        settings.TKM_WEBSERVICE_USER_LOGIN, settings.TKM_WEBSERVICE_USER_PASS))
     return getResponseFromTkmWebServices(json.loads(req.content))
     # reqJsonTMP = json.loads(req.content)
     # reqJson ={}
     # reqJson = reqJsonTMP['message'][0]
-    # reqJson['response'] = json.loads(reqJsonTMP['message'][0].get('response', '[]'))   
+    # reqJson['response'] = json.loads(reqJsonTMP['message'][0].get('response', '[]'))
     # return reqJson
+
 
 @login_required(login_url='/loginform/')
 def tkmGetPhotoList(request):
@@ -78,10 +83,11 @@ def tkmGetPhotoList(request):
     if isAutorized(request.user, permisions):
         photos = getFilesList(settings.PHOTO_DIR)
         return HttpResponse([photos], content_type='application/json')
-    return HttpResponse('Brak uprawnień:'+ ', '.join(permisions), content_type='application/json')
+    return HttpResponse('Brak uprawnień:' + ', '.join(permisions), content_type='application/json')
+
 
 @login_required(login_url='/loginform/')
-def tkmPdfExport(request):    
+def tkmPdfExport(request):
     permisions = settings.TKMSERVICES_PRINTCARD_PERMISIONS
     if isAutorized(request.user, permisions):
         datarequest = request.GET
@@ -91,22 +97,23 @@ def tkmPdfExport(request):
         text = datarequest.get('text', '')
         if(name == None or surname == None or id == None):
             raise MultiValueDictKeyError
-            
-        title = name +'_'+ surname +'.pdf'
+
+        title = name + '_' + surname + '.pdf'
         bufferedPdf = None
         try:
             bufferedPdf = pdfExport(name, surname, id, title, text)
             return FileResponse(bufferedPdf, as_attachment=True, filename=title+'.pdf')
-        except:        
+        except:
             logger.error(sys.exc_info())
-            return redirect('/tkmservices/printcard/?name='+str(name)+
-                '&surname='+str(surname)+'&id='+str(id)+'&text='+
-                    str(text)+'&error=BŁĄD EKSPORTU - SPRAWDŹ ZDJĘCIE !! :'+str(id))
-    return render(request, 'front/loginform.html', {'text':'Wymagane uprawnienia: '+ ', '.join(permisions), 'back': '/tkmservices'})
+            return redirect('/tkmservices/printcard/?name='+str(name) +
+                            '&surname='+str(surname)+'&id='+str(id)+'&text=' +
+                            str(text)+'&error=BŁĄD EKSPORTU - SPRAWDŹ ZDJĘCIE !! :'+str(id))
+    return render(request, 'front/loginform.html', {'text': 'Wymagane uprawnienia: ' + ', '.join(permisions), 'back': '/tkmservices'})
+
 
 @login_required(login_url='/loginform/')
 def tkmPdfMultipleExport(request):
-    permisions = settings.TKMSERVICES_PRINTCARD_PERMISIONS    
+    permisions = settings.TKMSERVICES_PRINTCARD_PERMISIONS
     if isAutorized(request.user, permisions):
         datarequest = request.GET
         startDate = datarequest['startdate']
@@ -117,34 +124,40 @@ def tkmPdfMultipleExport(request):
             raise MultiValueDictKeyError
         data = tkmGetCustomerFormData(startDate, endDate, variant, online)
 
-        title = 'Eksport '+ startDate +'-'+ endDate +', wariant '+ variant +', czy online '+ str(online) +'.pdf'
+        title = 'Eksport ' + startDate + '-' + endDate + ', wariant ' + \
+            variant + ', czy online ' + str(online) + '.pdf'
         bufferedPdf = None
         try:
             bufferedPdf = pdfMultiPagesExport(title, data)
             return FileResponse(bufferedPdf, as_attachment=True, filename=title+'.pdf')
         except:
             logger.error(sys.exc_info())
-            return redirect('/tkmservices/printcard/?error=BŁĄD EKSPORTU - SPRAWDŹ ZDJĘCIA !! :'+ startDate +'-'+ endDate)
-        return render(request, 'front/loginform.html', {'text':'Wymagane uprawnienia: '+ ', '.join(permisions), 'back': '/tkmservices'})
+            return redirect('/tkmservices/printcard/?error=BŁĄD EKSPORTU - SPRAWDŹ ZDJĘCIA !! :' + startDate + '-' + endDate)
+    return render(request, 'front/loginform.html', {'text': 'Wymagane uprawnienia: ' + ', '.join(permisions), 'back': '/tkmservices'})
+
 
 def getPhotoName(id):
     if not id:
         return 'błąd id'
     if id.find('.jpg') == -1:
         id += '.jpg'
-    return settings.PHOTO_DIR  + id
+    return settings.PHOTO_DIR + id
+
 
 def generateOnePagePdf(p, data, id):
     p.setFont("Arial-bold", FONT_SIZE)
-    p.drawString(TEXT_X, TEXT_Y, data['name'].upper()+'  '+data['surname'].upper())
+    p.drawString(TEXT_X, TEXT_Y,
+                 data['name'].upper()+'  '+data['surname'].upper())
     p.drawString(TEXT_X, TEXT_Y-FONT_H, data['text'].upper())
     p.drawInlineImage(getPhotoName(id), PHOTO_X, PHOTO_Y, PHOTO_W, PHOTO_H)
     p.showPage()
 
+
 def pdfExport(name, surname, id, title, text):
     # register arial bond font
     if settings.FONTS:
-        pdfmetrics.registerFont(TTFont('Arial-bold', settings.FONTS+'arial-bold.ttf'))
+        pdfmetrics.registerFont(
+            TTFont('Arial-bold', settings.FONTS+'arial-bold.ttf'))
     # Standard const params
     bufferedPdf = io.BytesIO()
     p = canvas.Canvas(bufferedPdf)
@@ -156,30 +169,46 @@ def pdfExport(name, surname, id, title, text):
     bufferedPdf.seek(0)
     return bufferedPdf
 
+
+
+
 def pdfMultiPagesExport(title, data):
     if settings.FONTS:
-        pdfmetrics.registerFont(TTFont('Arial-bold', settings.FONTS+'arial-bold.ttf'))
+        pdfmetrics.registerFont(
+            TTFont('Arial-bold', settings.FONTS+'arial-bold.ttf'))
     lostPhotos = []
-    bufferedPdf = io.BytesIO()    
+    bufferedPdf = io.BytesIO()
     p = canvas.Canvas(bufferedPdf)
     p.setPageSize((WIDTH, HEIGHT))
     p.setTitle(title)
+    persons = []
+    # analiza
     for d in data['response']:
         fileName = None
         id = d['id']
         if id:
             fileName = getPhotoName(id)
-        elif d['birthdate'] :
-            id = str(d['birthdate'])[0:FOREIGNER_ID_LENGTH]+ '_' +d['name'][0:1].upper()
+        elif d['birthdate']:
+            id = str(d['birthdate'])[0:FOREIGNER_ID_LENGTH] + \
+                '_' + d['name'][0:1].upper()
             fileName = getPhotoName(id)
-        if os.path.exists(fileName):
-            generateOnePagePdf(p, d, id)
+        # Dodanie daty utworzenia pliku w celu sortowania
+        if fileName != None and os.path.exists(fileName):
+            d['fileDate'] = os.path.getctime(fileName)
+            d['fileId'] = id
+            persons.append(d)
         else:
-            lostPhotos.append(id +'.jpg - '+ d['name'] +' '+ d['surname'])
+            lostPhotos.append(id + '.jpg - ' + d['name'] + ' ' + d['surname'])
+    # sortowanie po dacie pliku
+    if settings.SORT_MULTIPLE_PRINT_WITH_FILE_DATE :
+        persons.sort(key=lambda s: s['fileDate'])
+    # wydruk
+    for person in persons:
+        generateOnePagePdf(p, person, person['fileId'])
     p.setPageSize((210*mm, 297*mm))
     x = A4_TEXT_X
     y = A4_TEXT_Y
-    lostPhotos.append('Ilość: '+ str(len(lostPhotos)))
+    lostPhotos.append('Ilość: ' + str(len(lostPhotos)))
     for photo in lostPhotos:
         p.setFont("Arial-bold", FONT_SIZE)
         p.drawString(x, y, photo)
